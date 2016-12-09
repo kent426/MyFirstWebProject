12a79f6a773c677ead303104819cce2858dd7b36
Kent Huang <kenttttt0@gmail.com>
2016-12-09 12:42:07 -0800

    Write your commit message here

---
+++ myJavaScript.js
@@ -0,0 +1,4 @@
+function javaScriptFunction() {
+    document.getElementById("content").innerHTML = "Hello World from your first WebStrom project.";
+    document.getElementById("content").style.color = "blue";
+}
